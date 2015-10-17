46487a0b7e6ecc03637e1ef82c1168d61d42e1fc
Matthias Jahn <matthias.jahn@peerigon.com>
2015-10-17 13:25:41 +0200

    Add tests for emitted events

--- test/run.js
+++ test/run.js
@@ -1,15 +1,16 @@
 "use strict";
 
-var assert = require("assert");
-var run = require("../lib/run");
 var childProcess = require("child_process");
+var chai = require("chai");
+var run = require("../lib/run");
+
+var expect = chai.expect;
 
 var reporter;
-var test;
 var execBackup;
-var expectedNoOutdatedModule = {};
-var expectedOneOutdatedModule = {
-    "unicons": {
+var noOutdatedModules = {};
+var outdatedModules = {
+    unicons: {
         current: "0.1.4",
         wanted: "1.1.5",
         latest: "2.0.0",
@@ -17,343 +18,240 @@ var expectedOneOutdatedModule = {
         type: "dependencies"
     }
 };
+var outdatedModulesWithGitDependencies = {
+    unicons: outdatedModules.unicons,
+    "xunit-file": {
+        current: "0.0.7",
+        wanted: "git",
+        latest: "git",
+        location: "xunit-file",
+        type: "dependencies"
+    },
+    "servus.js": {
+        current: "0.0.1",
+        wanted: "git",
+        latest: "git",
+        location: "servus.js",
+        type: "dependencies"
+    }
+};
+
+var expectedOptions = {
+    infos: [{
+        current: "0.1.4",
+        wanted: "1.1.5",
+        latest: "2.0.0",
+        location: "unicons",
+        type: "dependencies",
+        name: "unicons",
+        saveCmd: "--save"
+    }],
+    total: 1
+};
+
+var expectedOptionsWithCurrentCount = {
+    current: 1,
+    total: 1,
+    info: {
+        current: "0.1.4",
+        wanted: "1.1.5",
+        latest: "2.0.0",
+        location: "unicons",
+        type: "dependencies",
+        name: "unicons",
+        saveCmd: "--save"
+    }
+};
+
+function tearDown() {
+    childProcess.exec = execBackup;
+}
+
+function execMock(object) {
+    return function (cmd, obj, cb) {
+        if (cmd === "npm outdated --json --long --depth=0") {
+            setImmediate(cb, null, JSON.stringify(object), null);
+        } else if (cmd === "npm test") {
+            setImmediate(cb, new Error());
+        } else {
+            setImmediate(cb, null);
+        }
+    };
+}
+
+function setupOutdatedModules(obj) {
+    return function () {
+        execBackup = childProcess.exec;
+        childProcess.exec = execMock(obj);
+    };
+}
 
 describe("run()", function () {
-    it("should throw an error if no options set", function () {
-        assert.throws(
-            function () {
-                run();
-            },
-            Error
-        );
-    });
-
-    it("should throw an error if cwd is no string", function () {
-        assert.throws(
-            function () {
-                run({
-                    cwd: 1
-                });
-            },
-            /Cannot run updtr: cwd missing/
-        );
-    });
-
     describe(".emit('init')", function () {
-        before(function () {
-            execBackup = childProcess.exec;
-            // cmd, { encoding: "utf8", cwd: config.cwd }, cb
-            test = function (cmd, obj, cb) {
-                // err, stdout, stderr
-                cb(null, JSON.stringify(expectedNoOutdatedModule), null);
-            };
-            childProcess.exec = test;
+        before(setupOutdatedModules(outdatedModules));
+
+        after(tearDown);
+
+        it("should throw an error, if no options set", function () {
+            expect(run).to.throw(Error);
         });
 
-        afterEach(function () {
-            childProcess.exec = execBackup;
+        it("should throw an error, if cwd is missing", function () {
+            expect(
+                function () {
+                    run({ cwd: 1 });
+                }
+            ).to.throw(Error);
         });
 
-        it("should be emitted", function (done) {
+        it("should be emitted, if cwd is set", function (done) {
             reporter = function (emitter) {
                 emitter.on("init", function (options) {
-                    assert.deepEqual(options, {
-                        cwd: "/Users/peerigon/Workspace/peerigon/updtr"
-                    });
+                    expect(options).to.eql({ cwd: "/Users/peerigon/Workspace/peerigon/updtr" });
                 });
             };
 
-            run({
-                cwd: process.cwd(),
-                reporter: reporter
-            }, done);
+            run({ cwd: process.cwd(), reporter: reporter }, done);
         });
     });
 
     describe(".emit('noop')", function () {
-        before(function () {
-            execBackup = childProcess.exec;
-            // cmd, { encoding: "utf8", cwd: config.cwd }, cb
-            test = function (cmd, obj, cb) {
-                // err, stdout, stderr
-                cb(null, JSON.stringify(expectedNoOutdatedModule), null);
-            };
-            childProcess.exec = test;
-        });
+        before(setupOutdatedModules(noOutdatedModules));
 
-        afterEach(function () {
-            childProcess.exec = execBackup;
-        });
+        afterEach(tearDown);
 
         it("should be emitted if no outdated modules were found", function (done) {
             reporter = function (emitter) {
                 emitter.on("noop", function (options) {
-                    assert.equal(options, undefined);
+                    expect(options).to.be.undefined; // eslint-disable-line
                 });
             };
 
-            run({
-                cwd: process.cwd(),
-                reporter: reporter
-            }, done);
+            run({ cwd: process.cwd(), reporter: reporter }, done);
         });
     });
 
     describe(".emit('outdated')", function () {
-        beforeEach(function () {
-            execBackup = childProcess.exec;
-            // cmd, { encoding: "utf8", cwd: config.cwd }, cb
-            test = function (cmd, obj, cb) {
-                // err, stdout, stderr
-                cb(null, JSON.stringify(expectedOneOutdatedModule), null);
-            };
-            childProcess.exec = test;
+        describe("without git modules", function () {
+            before(setupOutdatedModules(outdatedModules));
+
+            afterEach(tearDown);
+
+            it("should be emitted if outdated modules were found", function (done) {
+                reporter = function (emitter) {
+                    emitter.on("outdated", function (options) {
+                        expect(options).to.eql(expectedOptions);
+                    });
+                };
+
+                run({ cwd: process.cwd(), reporter: reporter }, done);
+            });
         });
 
-        it("should be emitted if outdated modules were found", function (done) {
-            reporter = function (emitter) {
-                emitter.on("outdated", function (options) {
-                    assert.deepEqual(options, {
-                        infos: [{
-                            current: "0.1.4",
-                            wanted: "1.1.5",
-                            latest: "2.0.0",
-                            location: "unicons",
-                            type: "dependencies",
-                            name: "unicons",
-                            saveCmd: "--save"
-                        }],
-                        total: 1
-                    });
-                });
-            };
+        describe("whit git modules", function () {
+            before(setupOutdatedModules(outdatedModulesWithGitDependencies));
 
-            run({
-                cwd: process.cwd(),
-                reporter: reporter
-            }, done);
+            afterEach(tearDown);
+
+            it("should be emitted without git modules", function (done) {
+                reporter = function (emitter) {
+                    emitter.on("outdated", function (options) {
+                        expect(options).to.eql(expectedOptions);
+                    });
+                };
+
+                run({ cwd: process.cwd(), reporter: reporter }, done);
+            });
         });
     });
 
     describe(".emit('updating')", function () {
-        beforeEach(function () {
-            execBackup = childProcess.exec;
-            // cmd, { encoding: "utf8", cwd: config.cwd }, cb
-            test = function (cmd, obj, cb) {
-                // err, stdout, stderr
-                cb(null, JSON.stringify(expectedOneOutdatedModule), null);
-            };
-            childProcess.exec = test;
-        });
+        before(setupOutdatedModules(outdatedModules));
+        afterEach(tearDown);
 
         it("should be emitted if outdated modules were found", function (done) {
             reporter = function (emitter) {
                 emitter.on("updating", function (options) {
-                    assert.deepEqual(options, {
-                        current: 1,
-                        total: 1,
-                        info: {
-                            current: "0.1.4",
-                            wanted: "1.1.5",
-                            latest: "2.0.0",
-                            location: "unicons",
-                            type: "dependencies",
-                            name: "unicons",
-                            saveCmd: "--save"
-                        }
-                    });
+                    expect(options).to.eql(expectedOptionsWithCurrentCount);
                 });
             };
 
-            run({
-                cwd: process.cwd(),
-                reporter: reporter
-            }, done);
+            run({ cwd: process.cwd(), reporter: reporter }, done);
         });
     });
 
     describe(".emit('testing')", function () {
-        beforeEach(function () {
-            execBackup = childProcess.exec;
-            // cmd, { encoding: "utf8", cwd: config.cwd }, cb
-            test = function (cmd, obj, cb) {
-                // err, stdout, stderr
-                cb(null, JSON.stringify(expectedOneOutdatedModule), null);
-            };
-            childProcess.exec = test;
-        });
+        before(setupOutdatedModules(outdatedModules));
+        afterEach(tearDown);
 
         it("should be emitted if outdated modules were found", function (done) {
             reporter = function (emitter) {
                 emitter.on("testing", function (options) {
-                    assert.deepEqual(options, {
-                        current: 1,
-                        total: 1,
-                        info: {
-                            current: "0.1.4",
-                            wanted: "1.1.5",
-                            latest: "2.0.0",
-                            location: "unicons",
-                            type: "dependencies",
-                            name: "unicons",
-                            saveCmd: "--save"
-                        }
-                    });
+                    expect(options).to.eql(expectedOptionsWithCurrentCount);
                 });
             };
 
-            run({
-                cwd: process.cwd(),
-                reporter: reporter
-            }, done);
+            run({ cwd: process.cwd(), reporter: reporter }, done);
         });
     });
 
     describe(".emit('rollback')", function () {
-        beforeEach(function () {
-            execBackup = childProcess.exec;
-            // cmd, { encoding: "utf8", cwd: config.cwd }, cb
-            test = function (cmd, obj, cb) {
-                // err, stdout, stderr
-                if (cmd === "npm outdated --json --long --depth=0") {
-                    cb(null, JSON.stringify(expectedOneOutdatedModule), null);
-                } else if (cmd === "npm test") {
-                    cb(new Error);
-                } else {
-                    cb(null);
-                }
-            };
-            childProcess.exec = test;
-        });
+        before(setupOutdatedModules(outdatedModules));
+        afterEach(tearDown);
 
         it("should be emitted if outdated modules were found", function (done) {
             reporter = function (emitter) {
                 emitter.on("rollback", function (options) {
-                    assert.deepEqual(options, {
-                        current: 1,
-                        total: 1,
-                        info: {
-                            current: "0.1.4",
-                            wanted: "1.1.5",
-                            latest: "2.0.0",
-                            location: "unicons",
-                            type: "dependencies",
-                            name: "unicons",
-                            saveCmd: "--save"
-                        }
-                    });
+                    expect(options).to.eql(expectedOptionsWithCurrentCount);
                 });
             };
 
-            run({
-                cwd: process.cwd(),
-                reporter: reporter
-            }, done);
+            run({ cwd: process.cwd(), reporter: reporter }, done);
         });
     });
 
     describe(".emit('rollbackDone')", function () {
-        beforeEach(function () {
-            execBackup = childProcess.exec;
-            // cmd, { encoding: "utf8", cwd: config.cwd }, cb
-            test = function (cmd, obj, cb) {
-                // err, stdout, stderr
-                if (cmd === "npm outdated --json --long --depth=0") {
-                    cb(null, JSON.stringify(expectedOneOutdatedModule), null);
-                } else if (cmd === "npm test") {
-                    cb(new Error);
-                } else {
-                    cb(null);
-                }
-            };
-            childProcess.exec = test;
-        });
+        before(setupOutdatedModules(outdatedModules));
+        afterEach(tearDown);
 
         it("should be emitted if outdated modules were found", function (done) {
             reporter = function (emitter) {
                 emitter.on("rollbackDone", function (options) {
-                    assert.deepEqual(options, {
-                        current: 1,
-                        total: 1,
-                        info: {
-                            current: "0.1.4",
-                            wanted: "1.1.5",
-                            latest: "2.0.0",
-                            location: "unicons",
-                            type: "dependencies",
-                            name: "unicons",
-                            saveCmd: "--save"
-                        }
-                    });
+                    expect(options).to.eql(expectedOptionsWithCurrentCount);
                 });
             };
 
-            run({
-                cwd: process.cwd(),
-                reporter: reporter
-            }, done);
+            run({ cwd: process.cwd(), reporter: reporter }, done);
         });
     });
 
     describe(".emit('updatingDone')", function () {
-        beforeEach(function () {
-            execBackup = childProcess.exec;
-            // cmd, { encoding: "utf8", cwd: config.cwd }, cb
-            test = function (cmd, obj, cb) {
-                // err, stdout, stderr
-                cb(null, JSON.stringify(expectedOneOutdatedModule), null);
-            };
-            childProcess.exec = test;
-        });
+        before(setupOutdatedModules(outdatedModules));
+        afterEach(tearDown);
 
         it("should be emitted if outdated modules were found", function (done) {
             reporter = function (emitter) {
                 emitter.on("updatingDone", function (options) {
-                    assert.deepEqual(options, {
-                        current: 1,
-                        total: 1,
-                        info: {
-                            current: "0.1.4",
-                            wanted: "1.1.5",
-                            latest: "2.0.0",
-                            location: "unicons",
-                            type: "dependencies",
-                            name: "unicons",
-                            saveCmd: "--save"
-                        }
-                    });
+                    expect(options).to.eql(expectedOptionsWithCurrentCount);
                 });
             };
 
-            run({
-                cwd: process.cwd(),
-                reporter: reporter
-            }, done);
+            run({ cwd: process.cwd(), reporter: reporter }, done);
         });
     });
 
     describe(".emit('finished')", function () {
-        beforeEach(function () {
-            execBackup = childProcess.exec;
-            // cmd, { encoding: "utf8", cwd: config.cwd }, cb
-            test = function (cmd, obj, cb) {
-                // err, stdout, stderr
-                cb(null, JSON.stringify(expectedOneOutdatedModule), null);
-            };
-            childProcess.exec = test;
-        });
+        before(setupOutdatedModules(outdatedModules));
+        afterEach(tearDown);
 
         it("should be emitted if outdated modules were found", function (done) {
             reporter = function (emitter) {
                 emitter.on("finished", function (options) {
-                    assert.deepEqual(options, undefined);
+                    expect(options).to.be.undefined; // eslint-disable-line
                 });
             };
 
-            run({
-                cwd: process.cwd(),
-                reporter: reporter
-            }, done);
+            run({ cwd: process.cwd(), reporter: reporter }, done);
         });
     });
 });
