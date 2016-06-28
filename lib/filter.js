peerigon/updtr

commit 18f7a8dd968630f9a7c12393f905fda78f60e8a4

Fix linting errors

lib/filter.js
@@ -1,0 +1,40 @@
+"use strict";
+
+var semver = require("semver");
+
+function isGitDependency(info) {
+    return info.latest === "git";
+}
+
+function isAlreadyInstalled(info) {
+    return info.updateTo === info.current;
+}
+
+function isUnstable(info) {
+    // Check for hyphen in version string as this is a prerelease version according to SemVer
+    return info.updateTo && info.updateTo.indexOf("-") !== -1;
+}
+
+function isExcluded(info, exclude) {
+    var excludedModules = exclude.map(function (name) {
+        return name.trim();
+    });
+
+    return info.name.match(new RegExp("^(" + excludedModules.join("|") + ")$"));
+}
+
+function isCurrentGreaterThanUpdateTo(info) {
+    return info.current && info.updateTo !== "git" && semver.gt(info.current, info.updateTo);
+}
+
+function filter(config) {
+    return function (info) {
+        return isGitDependency(info) === false &&
+            isAlreadyInstalled(info) === false &&
+            isUnstable(info) === false &&
+            isExcluded(info, config.exclude) === false &&
+            isCurrentGreaterThanUpdateTo(info) === false;
+    };
+}
+
+module.exports = filter;

test/run.js
@@ -35,20 +35,6 @@ var outdatedModulesNotInstalled = {
     }
 };
 
-var expectedOptionsNotInstalled = {
-    infos: [{
-        current: "0.1.4",
-        wanted: "1.1.5",
-        latest: "2.0.0",
-        location: "unicons",
-        type: "dependencies",
-        name: "unicons",
-        saveCmd: "--save",
-        updateTo: "2.0.0"
-    }],
-    total: 1
-};
-
 var outdatedModulesExclude = {
     "servus.js": {
         current: "1.1.4",
@@ -169,7 +155,7 @@ var outdatedModulesWithVersionGreaterThanLatestInstalled = {
         latest: "2.0.0",
         location: "xunit-file",
         type: "dependencies"
-    },  
+    },
     unicons: {
         current: "0.1.0",
         wanted: "0.0.5",
@@ -300,7 +286,7 @@ var expectedOptionsWithCurrentCountWantedAndSpecifiedRegistry = {
         type: "dependencies",
         name: "unicons",
         saveCmd: "--save",
-        updateTo: "1.1.5",
+        updateTo: "1.1.5"
     },
     testCmd: "npm test",
     installCmd: "npm i --registry https://curstom.npm.registry"

lib/run.js
@@ -2,16 +2,24 @@
 
 var async = require("async");
 var childProcess = require("child_process");
-var semver = require("semver");
-
+var filter = require("./filter");
 var EventEmitter = require("./EventEmitter.js");
 
-var testCmd = "npm test";
+var defaultTestCmd = "npm test";
+
+function sanitizeConfig(config) {
+    config.reporter = config.reporter || Function.prototype;
+    config.testCmd = config.testCmd || defaultTestCmd;
+    config.exclude = config.exclude && config.exclude.split(",").map(function (name) {
+        return name.trim();
+    }) || [];
+}
 
 function run(config, done) {
     var emitter = new EventEmitter();
-    var reporter = config.reporter || Function.prototype;
-    var cwd = config.cwd;
+    var testCmd;
+    var reporter;
+    var cwd;
 
     function exec(cmd, cb) {
         childProcess.exec(cmd, { encoding: "utf8", cwd: config.cwd }, cb);
@@ -22,11 +30,15 @@ function run(config, done) {
         done(err || null);
     }
 
+    sanitizeConfig(config);
+
+    reporter = config.reporter;
+    cwd = config.cwd;
+    testCmd = config.testCmd;
+
     if (typeof cwd !== "string") {
         throw new Error("Cannot run updtr: cwd missing");
     }
-
-    testCmd = config.testCmd ? config.testCmd : testCmd;
 
     reporter(emitter);
 
@@ -70,7 +82,8 @@ function run(config, done) {
                         exec(testCmd, function (err, stdout, stderr) {
                             if (err) {
                                 testStdout = stdout;
-                                return callback(err);
+                                callback(err);
+                                return;
                             }
                             callback(null, stdout, stderr);
                         });
@@ -120,32 +133,7 @@ function run(config, done) {
 
                 return info;
             })
-            .filter(function (info) {
-                var isGitDependency = info.latest === "git";
-                var alreadyInstalled = info.updateTo === info.current;
-                var isUnstable = false;
-                var excludedModules;
-                var isExcluded = false;
-                var isCurrentGraterThanUpdateTo = false;
-
-                if (config.exclude) {
-                    excludedModules = config.exclude.split(",").map(function (name) {
-                        return name.trim();
-                    });
-                    isExcluded = info.name.match(new RegExp("^(" + excludedModules.join("|") + ")$"));
-                }
-
-                // Check for hyphen in version string as this is a prerelease version according to SemVer
-                if (info.updateTo && info.updateTo.indexOf("-") !== -1) {
-                    isUnstable = true;
-                }
-
-                if (info.current && info.updateTo !== "git" && semver.gt(info.current, info.updateTo)) {
-                    isCurrentGraterThanUpdateTo = true;
-                }
-
-                return !(isGitDependency || alreadyInstalled || isUnstable || isExcluded || isCurrentGraterThanUpdateTo);
-            });
+            .filter(filter(config));
 
         modulesMissing = infos.some(function (info) {
             return !info.current;

lib/index.js
@@ -3,8 +3,4 @@
 var run = require("./run.js");
 
 exports.run = run;
-exports.reporters = {
-    default: require("./reporters/default.js"),
-    shy: require("./reporters/shy.js"),
-    simple: require("./reporters/simple.js")
-};
+exports.reporters = require("./reporters");

lib/reporters/index.js
@@ -1,3 +1,7 @@
 "use strict";
 
-exports.default = require("./default.js");
+module.exports = {
+    default: require("./default.js"),
+    shy: require("./shy.js"),
+    simple: require("./simple.js")
+};

lib/reporters/simple.js
@@ -72,16 +72,18 @@ function defaultReporter(emitter) {
     });
     emitter.on("rollbackDone", function (event) {
         var info = event.info;
+
         logUpdateProgress(event, "error", info.updateTo + " failed".grey);
-		console.log("");
+        console.log("");
     });
     emitter.on("testStdout", function (event) {
         console.log(event.testStdout);
     });
     emitter.on("updatingDone", function (event) {
         var info = event.info;
+
         logUpdateProgress(event, "success", info.updateTo + " success".grey);
-		console.log("");
+        console.log("");
     });
     emitter.on("finished", function () {
         console.log("");

commit 58a359f5589446d84e4e76aefc56307babcb4a4e

Fix isExcluded.
18f7a8dd968 introduced a broken `isExcluded()’. `match()’ returns an
array with matches or null if none was found. So `isExcluded’ never
returned false.

We now use `indexOf()’ instead of constructing a RegExp.

lib/filter.js
@@ -16,11 +16,9 @@ function isUnstable(info) {
 }
 
 function isExcluded(info, exclude) {
-    var excludedModules = exclude.map(function (name) {
+    return exclude.map(function (name) {
         return name.trim();
-    });
-
-    return info.name.match(new RegExp("^(" + excludedModules.join("|") + ")$"));
+    }).indexOf(info.name) !== -1;
 }
 
 function isCurrentGreaterThanUpdateTo(info) {

test/run.js
@@ -629,7 +629,7 @@ describe("run()", function () {
                         });
                     };
 
-                    run({ cwd: process.cwd(), reporter: reporter, testStdout: true }, done);
+                    run({ cwd: process.cwd(), reporter: reporter, testStdout: true, exclude: "unicons" }, done);
                 });
             });
         });