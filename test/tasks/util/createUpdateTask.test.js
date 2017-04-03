0d84eb7f913f97ee316406619ff55d062dcb38f4
Johannes Ewald <johannes.ewald@peerigon.com>
2017-04-03 21:50:59 +0200

    Refactor tests

---
+++ test/tasks/util/createUpdateTask.test.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const dependencyTypes = require("../../../lib/dependencyTypes");
+const createUpdateTask = require("../../../lib/tasks/util/createUpdateTask");
+
+const baseOutdated = {
+    name: "some-module",
+    type: dependencyTypes.REGULAR,
+    current: "1.0.0",
+    latest: "2.0.0",
+    wanted: "1.8.0",
+};
+const baseInstanceConfig = {
+    updateTo: "latest",
+};
+
+describe("createUpdateTask()", () => {
+    test("should return a valid update task", () => {
+        expect(
+            createUpdateTask(baseOutdated, baseInstanceConfig)
+        ).toMatchSnapshot();
+    });
+    describe("if the 'wanted' flag is set", () => {
+        test("should return a valid update task", () => {
+            const instanceConfig = Object.assign({}, baseInstanceConfig);
+
+            instanceConfig.updateTo = "wanted";
+
+            expect(
+                createUpdateTask(baseOutdated, instanceConfig)
+            ).toMatchSnapshot();
+        });
+    });
+});
--- test/tasks/util/filterUpdateTask.test.js
+++ test/tasks/util/filterUpdateTask.test.js
@@ -3,83 +3,82 @@
 const filterUpdateTask = require("../../../lib/tasks/util/filterUpdateTask");
 const dependencyTypes = require("../../../lib/dependencyTypes");
 
-const baseUpdateTaskMock = {
+const baseUpdateTask = {
     name: "some-module",
     type: dependencyTypes.REGULAR,
     updateTo: "2.0.0",
     rollbackTo: "1.0.0",
 };
-const baseInstanceConfigMock = {
+const baseInstanceConfig = {
     exclude: [],
 };
 
 describe("filterUpdateTask()", () => {
     test("should not filter a regular update task", () => {
-        const updateTask = Object.assign({}, baseUpdateTaskMock);
-
-        expect(filterUpdateTask(updateTask, baseInstanceConfigMock)).toBe(true);
+        expect(filterUpdateTask(baseUpdateTask, baseInstanceConfig)).toBe(true);
     });
     describe("exotic dependencies", () => {
         test("should filter git dependencies", () => {
-            const updateTask = Object.assign({}, baseUpdateTaskMock);
+            const updateTask = Object.assign({}, baseUpdateTask);
 
             updateTask.updateTo = "git";
 
-            expect(filterUpdateTask(updateTask, baseInstanceConfigMock)).toBe(
+            expect(filterUpdateTask(updateTask, baseInstanceConfig)).toBe(
                 false
             );
         });
         test("should filter exotic dependencies", () => {
-            const updateTask = Object.assign({}, baseUpdateTaskMock);
+            const updateTask = Object.assign({}, baseUpdateTask);
 
             updateTask.updateTo = "exotic";
 
-            expect(filterUpdateTask(updateTask, baseInstanceConfigMock)).toBe(
+            expect(filterUpdateTask(updateTask, baseInstanceConfig)).toBe(
                 false
             );
         });
     });
     describe("unstable dependencies", () => {
         test("should filter pre-releases according to semver", () => {
-            const updateTask = Object.assign({}, baseUpdateTaskMock);
+            const updateTask = Object.assign({}, baseUpdateTask);
 
             updateTask.updateTo = "2.0.0-alpha";
-            expect(filterUpdateTask(updateTask, baseInstanceConfigMock)).toBe(
+            expect(filterUpdateTask(updateTask, baseInstanceConfig)).toBe(
                 false
             );
             updateTask.updateTo = "2.0.0-beta";
-            expect(filterUpdateTask(updateTask, baseInstanceConfigMock)).toBe(
+            expect(filterUpdateTask(updateTask, baseInstanceConfig)).toBe(
                 false
             );
             updateTask.updateTo = "2.0.0-some-other-cryptic-thing";
-            expect(filterUpdateTask(updateTask, baseInstanceConfigMock)).toBe(
+            expect(filterUpdateTask(updateTask, baseInstanceConfig)).toBe(
                 false
             );
         });
     });
     describe("excluded dependencies", () => {
         test("should honor the given exclude filter", () => {
-            const updateTask = Object.assign({}, baseUpdateTaskMock);
-            const instanceConfig = Object.assign({}, baseInstanceConfigMock);
+            const instanceConfig = Object.assign({}, baseInstanceConfig);
 
-            instanceConfig.exclude = [updateTask.name];
-            expect(filterUpdateTask(updateTask, instanceConfig)).toBe(false);
+            instanceConfig.exclude = [baseUpdateTask.name];
+            expect(filterUpdateTask(baseUpdateTask, instanceConfig)).toBe(
+                false
+            );
         });
     });
     describe("inconsistent update tasks", () => {
         test("should filter if rollbackTo is the same as updateTo", () => {
-            const updateTask = Object.assign({}, baseUpdateTaskMock);
+            const updateTask = Object.assign({}, baseUpdateTask);
 
             updateTask.rollbackTo = "2.0.0";
-            expect(filterUpdateTask(updateTask, baseInstanceConfigMock)).toBe(
+            expect(filterUpdateTask(updateTask, baseInstanceConfig)).toBe(
                 false
             );
         });
         test("should filter if rollbackTo is greater than updateTo", () => {
-            const updateTask = Object.assign({}, baseUpdateTaskMock);
+            const updateTask = Object.assign({}, baseUpdateTask);
 
             updateTask.rollbackTo = "3.0.0";
-            expect(filterUpdateTask(updateTask, baseInstanceConfigMock)).toBe(
+            expect(filterUpdateTask(updateTask, baseInstanceConfig)).toBe(
                 false
             );
         });
