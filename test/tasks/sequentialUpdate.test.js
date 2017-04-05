fef1488a40aa3dabe5836a3775cd93fdd70aaf75
Johannes Ewald <johannes.ewald@peerigon.com>
2017-04-05 16:12:23 +0200

    sequentialUpdate test refactor

--- test/tasks/sequentialUpdate.test.js
+++ test/tasks/sequentialUpdate.test.js
@@ -35,51 +35,45 @@ describe("sequentialUpdate()", () => {
     });
     describe("when the given updateTasks array contains update tasks", () => {
         describe("using npm", () => {
-            describe("when the wanted flag is not set", () => {
-                describe("when the tests succeed", () => {
-                    test("should emit expected events and execute expected commands", async () => {
-                        const updtr = new FakeUpdtr({
-                            wanted: false,
-                        });
-                        const updateTasks = createUpdateTasks(updtr.config);
+            describe("when the tests succeed", () => {
+                test("should emit expected events and execute expected commands", async () => {
+                    const updtr = new FakeUpdtr();
+                    const updateTasks = createUpdateTasks(updtr.config);
 
-                        updtr.execResults = [
+                    updtr.execResults = [
+                        Promise.resolve({ stdout: "" }), // update
+                        Promise.resolve({ stdout: "Everything ok" }), // testing
+                    ];
+
+                    // Truncate the updateTasks for this test because we only want to test the sequence
+                    // of one update task. This makes it easier to read and compare the snapshot.
+                    updateTasks.length = 1;
+                    await sequentialUpdate(updtr, updateTasks);
+
+                    expect(updtr.execArgs).toMatchSnapshot();
+                    expect(updtr.emittedEvents).toMatchSnapshot();
+                });
+                test("should return the expected update results", async () => {
+                    const updtr = new FakeUpdtr();
+                    const updateTasks = createUpdateTasks(updtr.config);
+
+                    updtr.execResults = updateTasks.reduce(execResults =>
+                        execResults.concat(
                             Promise.resolve({ stdout: "" }), // update
-                            Promise.resolve({ stdout: "Everything ok" }), // testing
-                        ];
+                            Promise.resolve({ stdout: "Everything ok" }) // testing
+                        ), []);
 
-                        // Truncate the updateTasks for this test because we only want to test the sequence
-                        // of one update task. This makes it easier to read and compare the snapshot.
-                        updateTasks.length = 1;
-                        await sequentialUpdate(updtr, updateTasks);
+                    const updateResults = await sequentialUpdate(
+                        updtr,
+                        updateTasks
+                    );
 
-                        expect(updtr.execArgs).toMatchSnapshot();
-                        expect(updtr.emittedEvents).toMatchSnapshot();
-                    });
-                    test("should return the expected update results", async () => {
-                        const updtr = new FakeUpdtr({
-                            wanted: false,
-                        });
-                        const updateTasks = createUpdateTasks(updtr.config);
-
-                        updtr.execResults = updateTasks.reduce(execResults =>
-                            execResults.concat(
-                                Promise.resolve({ stdout: "" }), // update
-                                Promise.resolve({ stdout: "Everything ok" }) // testing
-                            ), []);
-
-                        const updateResults = await sequentialUpdate(
-                            updtr,
-                            updateTasks
+                    expect(updateResults).toMatchSnapshot();
+                    expect(updateResults.length).toBe(2);
+                    updateResults.forEach((updateResult, index) => {
+                        expect(updateResult.current).toBe(
+                            updateTasks[index].updateTo
                         );
-
-                        expect(updateResults).toMatchSnapshot();
-                        expect(updateResults.length).toBe(2);
-                        updateResults.forEach((updateResult, index) => {
-                            expect(updateResult.current).toBe(
-                                updateTasks[index].updateTo
-                            );
-                        });
                     });
                 });
             });
