f7b039340c8789741210a4510bdb74295e5491b9
Johannes Ewald <johannes.ewald@peerigon.com>
2017-04-05 16:34:11 +0200

    Add yarn tests to sequentialUpdate

--- test/tasks/sequentialUpdate.test.js
+++ test/tasks/sequentialUpdate.test.js
@@ -113,5 +113,89 @@ describe("sequentialUpdate()", () => {
                 });
             });
         });
+        describe("using yarn", () => {
+            describe("when the tests succeed", () => {
+                test("should emit expected events and execute expected commands", async () => {
+                    const updtr = new FakeUpdtr({
+                        packageManager: "yarn",
+                    });
+                    const updateTasks = createUpdateTasks(updtr.config);
+
+                    updtr.execResults = [
+                        Promise.resolve({ stdout: "" }), // update
+                        Promise.resolve({ stdout: "Everything ok" }), // test
+                    ];
+
+                    // Truncate the updateTasks for this test because we only want to test the sequence
+                    // of one update task. This makes it easier to read and compare the snapshot.
+                    updateTasks.length = 1;
+                    await sequentialUpdate(updtr, updateTasks);
+
+                    expect(updtr.execArgs).toMatchSnapshot();
+                    expect(updtr.emittedEvents).toMatchSnapshot();
+                });
+                test("should return the expected update results", async () => {
+                    const updtr = new FakeUpdtr({
+                        packageManager: "yarn",
+                    });
+                    const updateTasks = createUpdateTasks(updtr.config);
+
+                    updtr.execResults = [
+                        Promise.resolve({ stdout: "" }), // update
+                        Promise.resolve({ stdout: "Everything ok" }), // test
+                        Promise.resolve({ stdout: "" }), // update
+                        Promise.resolve({ stdout: "Everything ok" }), // testing
+                    ];
+
+                    const updateResults = await sequentialUpdate(
+                        updtr,
+                        updateTasks
+                    );
+
+                    expect(updateResults).toMatchSnapshot();
+                    // Additional sanity check since comparing version numbers in the snapshot can be error prone
+                    expect(updateResults[0].current).toBe(
+                        updateTasks[0].updateTo
+                    );
+                    expect(updateResults[1].current).toBe(
+                        updateTasks[1].updateTo
+                    );
+                });
+            });
+            describe("when the first test fails and the rest succeeds", () => {
+                test("should emit expected events and execute expected commands", async () => {
+                    const execErr = new ExecError({
+                        stdout: "Oh noez",
+                        exitCode: 1,
+                    });
+                    const updtr = new FakeUpdtr({
+                        packageManager: "yarn",
+                    });
+                    const updateTasks = createUpdateTasks(updtr.config);
+
+                    updtr.execResults = [
+                        Promise.resolve({ stdout: "" }), // update
+                        Promise.reject(execErr), // test
+                        Promise.resolve({ stdout: "" }), // rollback
+                        Promise.resolve({ stdout: "" }), // update
+                        Promise.resolve({ stdout: "Everything ok" }), // testing
+                    ];
+
+                    const updateResults = await sequentialUpdate(
+                        updtr,
+                        updateTasks
+                    );
+
+                    expect(updateResults).toMatchSnapshot();
+                    // Additional sanity check since comparing version numbers in the snapshot can be error prone
+                    expect(updateResults[0].current).toBe(
+                        updateTasks[0].rollbackTo
+                    );
+                    expect(updateResults[1].current).toBe(
+                        updateTasks[1].updateTo
+                    );
+                });
+            });
+        });
     });
 });
