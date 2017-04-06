81995cb325042db8e8dc560fd42ff819b7cdf350
Johannes Ewald <johannes.ewald@peerigon.com>
2017-04-06 20:44:04 +0200

    Finish updateVersionRange

--- src/tasks/util/updateVersionRange.js
+++ src/tasks/util/updateVersionRange.js
@@ -1,42 +1,63 @@
-import semverUtils from "semver-utils";
 import semver from "semver";
 
-const matchPreservableRange = /^(~|>=|)\d+\.(\d+|x)\.(\d+|x)$/;
-const matchExactVersion = /^(\d+)\.(\d+)\.(\d+)$/;
+// Matches a semver version range that can be transformed to the new version
+const matchPreservableRange = /^(~|>=|)\d+\.(\d+|x|\*)\.(\d+|x|\*)$/;
+// Matches parts of an exact semver version (no range)
+const matchExactVersion = /^(\d+)\.(\d+)\.(\d+)(-[^\s]+)?$/;
 const matchNumber = /^\d+$/;
 
+/**
+ * Tries to apply the newMinVersion while maintaining the range (see https://github.com/peerigon/updtr/issues/47)
+ * This is kind of risky because there are tons of semver possibilities. That's why this
+ * function is very conservative in accepting semver ranges. If the range is not easily updatable,
+ * we opt-out to npm's default caret operator.
+ *
+ * @param {string} oldRange
+ * @param {string} newMinVersion
+ * @returns {string}
+ */
 export default function updateVersionRange(oldRange, newMinVersion) {
     if (semver.validRange(oldRange) === null) {
+        // Very rare, but just to be sure
         return "^" + newMinVersion;
     }
 
-    if (matchExactVersion.test(oldRange)) {
+    if (matchExactVersion.test(oldRange) === true) {
+        // The old version was pinned, so the new should also pinned
         return newMinVersion;
     }
 
     const oldMatch = oldRange.match(matchPreservableRange);
     const newMatch = newMinVersion.match(matchExactVersion);
 
-    if (oldMatch && newMatch) {
+    if (oldMatch !== null && newMatch !== null) {
         const oldOperator = oldMatch[1];
         const oldMinor = oldMatch[2];
         const oldPatch = oldMatch[3];
 
         if (matchNumber.test(oldMinor) === false) {
             newMatch[2] = oldMinor;
-            if (matchNumber.test(oldPatch) === false) {
-                newMatch[3] = oldPatch;
-            }
+            newMatch[3] = matchNumber.test(oldPatch) === true ?
+                oldMinor :
+                oldPatch;
         } else if (matchNumber.test(oldPatch) === false) {
             newMatch[3] = oldPatch;
         }
 
-        return oldOperator +
+        const newVersionRange = oldOperator +
             newMatch[1] +
             "." +
             newMatch[2] +
             "." +
             newMatch[3];
+
+        // This is kind of error prone so let's do a sanity check if everything's ok
+        if (
+            semver.validRange(newVersionRange) !== null &&
+            semver.satisfies(newMinVersion, newVersionRange) === true
+        ) {
+            return newVersionRange;
+        }
     }
 
     return "^" + newMinVersion;
--- test/tasks/util/updateVersionRange.test.js
+++ test/tasks/util/updateVersionRange.test.js
@@ -4,6 +4,7 @@ import versionRanges from "../../fixtures/versionRanges";
 describe("updateVersionRange()", () => {
     it("should match the expected new version range", () => {
         const newVersionRanges = versionRanges.map(oldVersionRange =>
+        // const newVersionRanges = [">=1.0.0"].map(oldVersionRange =>
             updateVersionRange(oldVersionRange, "5.0.0"));
         const comparisons = versionRanges.map(
             (oldVersionRange, index) =>
