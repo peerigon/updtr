f8d82e6a5af44e0c8dd5bbb079c2b5476da0df2c
Johannes Ewald <johannes.ewald@peerigon.com>
2017-04-08 00:29:41 +0200

    Add first implementation of createUpdatedPackageJson

--- src/tasks/util/createUpdatedPackageJson.js
+++ src/tasks/util/createUpdatedPackageJson.js
@@ -1,29 +1,53 @@
-function createUpdatedDependencyMap(oldDependencyMap, successfulUpdateResults) {
-    return successfulUpdateResults.reduce(
-        (newDependencyMap, updateResult) => {},
-        { ...oldDependencyMap }
-    );
-}
+import updateVersionRange from "./updateVersionRange";
+
+const dependencyTypes = [
+    "dependencies",
+    "devDependencies",
+    "optionalDependencies",
+];
 
 export default function createUpdatedPackageJson(
     oldPackageJson,
     updateResults
 ) {
-    const successfulUpdateResults = updateResults.filter(
+    const newPackageJson = { ...oldPackageJson };
+    const successfulUpdates = updateResults.filter(
         updateResult => updateResult.success === true
     );
+    const dependenciesToSave = successfulUpdates.slice();
 
-    return SUPPORTED.reduce(
-        (newPackageJson, type) => {
-            newPackageJson[type];
-        },
-        { ...oldPackageJson }
-    );
-    SUPPORTED.forEach();
-    const oldDependencies = oldPackageJson.dependencies;
-    const oldDevDependencies = oldPackageJson.devDependencies;
-    const oldOptionalDependencies = oldPackageJson.optionalDependencies;
+    dependencyTypes
+        .filter(type => oldPackageJson[type] !== undefined)
+        .forEach(type => {
+            const dependencies = oldPackageJson[type];
+            const newDependencies = {};
 
-    // return
-    //     .reduce(({ type }) => [type, name, version], { ...oldPackageJson });
+            Object.keys(dependencies).forEach(moduleName => {
+                const updateIndex = successfulUpdates.findIndex(
+                    ({ name }) => name === moduleName
+                );
+                const update = successfulUpdates[updateIndex];
+                const oldVersionRange = dependencies[moduleName];
+
+                newDependencies[moduleName] = update === undefined ?
+                    oldVersionRange :
+                    updateVersionRange(oldVersionRange, update.version);
+
+                dependenciesToSave.splice(updateIndex, 1);
+            });
+
+            newPackageJson[type] = newDependencies;
+        });
+
+    if (dependenciesToSave.length > 0) {
+        const dependencies = newPackageJson.dependencies || {};
+
+        dependenciesToSave.forEach(update => {
+            dependencies[update.name] = update.version;
+        });
+
+        newPackageJson.dependencies = dependencies;
+    }
+
+    return newPackageJson;
 }
--- test/tasks/util/createUpdatedPackageJson.test.js
+++ test/tasks/util/createUpdatedPackageJson.test.js
@@ -1,3 +1,4 @@
+import clone from "clone";
 import createUpdatedPackageJson
     from "../../../src/tasks/util/createUpdatedPackageJson";
 import {
@@ -6,20 +7,118 @@ import {
     testModule1Fail,
     testModule2Fail,
 } from "../../fixtures/updateResults";
-import {
-    ready as packageJsonsReady,
-    outdatedDev as oldPackageJsonDev,
-    outdatedRegular as oldPackageJson,
-} from "../../fixtures/packageJsons";
 
-beforeAll(() => packageJsonsReady);
+// Freezes the essential parts of the package.json to detect changes on the input object
+function cloneAndFreeze(packageJson) {
+    const cloned = clone(packageJson);
 
-describe.skip("createUpdatedPackageJson()", () => {
-    describe("with regular dependencies", () => {
+    Object.freeze(cloned.dependencies);
+    Object.freeze(cloned.devDependencies);
+    Object.freeze(cloned.optionalDependencies);
+
+    return Object.freeze(cloned);
+}
+
+describe("createUpdatedPackageJson()", () => {
+    it("should not create new fields by default", () => {
+        const updateResults = [];
+        const newPackageJson = createUpdatedPackageJson(
+            Object.freeze({}),
+            updateResults
+        );
+
+        expect(newPackageJson).toEqual({});
+    });
+    it("should not create new fields for failed update results", () => {
+        const updateResults = [testModule1Fail, testModule2Fail];
+        const newPackageJson = createUpdatedPackageJson(
+            Object.freeze({}),
+            updateResults
+        );
+
+        expect(newPackageJson).toEqual({});
+    });
+    describe("dependencies", () => {
         it("should write the versions of the successful updates to the package json", () => {
             const updateResults = [testModule1Success, testModule2Fail];
             const newPackageJson = createUpdatedPackageJson(
-                oldPackageJson,
+                cloneAndFreeze({
+                    dependencies: {
+                        "dep-1": "1.0.0",
+                        "dep-2": "1.0.0",
+                        "updtr-test-module-1": "1.0.0",
+                        "updtr-test-module-2": "1.0.0",
+                    },
+                }),
+                updateResults
+            );
+
+            expect(newPackageJson).toMatchSnapshot();
+        });
+    });
+    describe("devDependencies", () => {
+        it("should write the versions of the successful updates to the package json", () => {
+            const updateResults = [testModule1Success, testModule2Fail];
+            const newPackageJson = createUpdatedPackageJson(
+                cloneAndFreeze({
+                    devDependencies: {
+                        "dep-1": "1.0.0",
+                        "dep-2": "1.0.0",
+                        "updtr-test-module-1": "1.0.0",
+                        "updtr-test-module-2": "1.0.0",
+                    },
+                }),
+                updateResults
+            );
+
+            expect(newPackageJson).toMatchSnapshot();
+        });
+    });
+    describe("optionalDependencies", () => {
+        it("should write the versions of the successful updates to the package json", () => {
+            const updateResults = [testModule1Success, testModule2Fail];
+            const newPackageJson = createUpdatedPackageJson(
+                cloneAndFreeze({
+                    optionalDependencies: {
+                        "dep-1": "1.0.0",
+                        "dep-2": "1.0.0",
+                        "updtr-test-module-1": "1.0.0",
+                        "updtr-test-module-2": "1.0.0",
+                    },
+                }),
+                updateResults
+            );
+
+            expect(newPackageJson).toMatchSnapshot();
+        });
+    });
+    // This is usually an error, but we don't care
+    describe("when a library is listed in more than one dependency map", () => {
+        it("should update all dependency maps", () => {
+            const updateResults = [testModule1Success];
+            const newPackageJson = createUpdatedPackageJson(
+                cloneAndFreeze({
+                    dependencies: {
+                        "updtr-test-module-1": "1.0.0",
+                    },
+                    devDependencies: {
+                        "updtr-test-module-1": "1.0.0",
+                    },
+                    optionalDependencies: {
+                        "updtr-test-module-1": "1.0.0",
+                    },
+                }),
+                updateResults
+            );
+
+            expect(newPackageJson).toMatchSnapshot();
+        });
+    });
+    describe("when the updated libraries are not listed in any dependency map", () => {
+        it("should save the modules as regular dependency", () => {
+            const updateResults = [testModule1Success, testModule2Success];
+            const newPackageJson = createUpdatedPackageJson(
+                Object.freeze({}),
                 updateResults
             );
 
