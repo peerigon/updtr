9c82b7959b635e4d8f105da8afc27458d7d2e7f3
Johannes Ewald <johannes.ewald@peerigon.com>
2017-04-10 19:51:40 +0200

    Improve run implementation

--- src/run.js
+++ src/run.js
@@ -4,45 +4,33 @@ import splitUpdateTasks from "./tasks/util/splitUpdateTasks";
 import batchUpdate from "./tasks/batchUpdate";
 import createUpdateResult from "./tasks/util/createUpdateResult";
 
-async function runBatchUpdate(updtr, nonBreaking) {
-    const batchSuccess = await batchUpdate(updtr, nonBreaking);
-    const batchUpdateWasSufficient = batchSuccess === true ||
-        nonBreaking.length < 2;
-
-    return batchUpdateWasSufficient === true ?
-        nonBreaking.map(updateTask =>
-              createUpdateResult(updateTask, batchSuccess)) :
-        [];
-}
-
-function runSequentialUpdate(updtr, batchUpdateResults, nonBreaking, breaking) {
-    const batchUpdateWasSufficient = batchUpdateResults.length ===
-        nonBreaking.length;
-
-    return sequentialUpdate(
-        updtr,
-        (batchUpdateWasSufficient === true ? [] : nonBreaking).concat(breaking)
-    );
-}
-
 export default (async function run(updtr) {
+    const results = [];
+    let batchSuccess = false;
+
     updtr.emit("start", {
         config: updtr.config,
     });
 
     const { updateTasks } = await init(updtr);
     const { breaking, nonBreaking } = splitUpdateTasks(updateTasks);
+    const sequentialUpdateTasks = breaking.slice();
 
-    const batchUpdateResults = await runBatchUpdate(updtr, nonBreaking);
-    const sequentialUpdateResults = await runSequentialUpdate(
-        updtr,
-        batchUpdateResults,
-        nonBreaking,
-        breaking
-    );
+    if (nonBreaking.length > 1) {
+        batchSuccess = await batchUpdate(updtr, nonBreaking);
+    }
+    if (batchSuccess === true) {
+        results.push(
+            ...nonBreaking.map(updateTask =>
+                createUpdateResult(updateTask, true))
+        );
+    } else {
+        sequentialUpdateTasks.unshift(...nonBreaking);
+    }
+    results.push(...(await sequentialUpdate(updtr, sequentialUpdateTasks)));
 
     updtr.emit("end", {
         config: updtr.config,
-        results: batchUpdateResults.concat(sequentialUpdateResults),
+        results,
     });
 });
--- test/run.test.js
+++ test/run.test.js
@@ -1,4 +1,3 @@
-import clone from "clone";
 import run from "../src/run";
 import FakeUpdtr from "./helpers/FakeUpdtr";
 import pickEventNames from "./helpers/pickEventNames";
@@ -7,39 +6,47 @@ import ExecError from "./helpers/ExecError";
 import {
     ready as execResultsReady,
     npmNoOutdated,
-    npmOutdated,
     updateTestPass,
     updateTestFail,
 } from "./fixtures/execResults";
 
-function npmOutdatedBatchUpdate() {
+function npmOutdated({ nonBreaking = 0, breaking = 0 }) {
+    const outdated = {};
+    let i;
+
+    for (i = 0; i < nonBreaking; i++) {
+        const name = "updtr-test-module-" + i;
+
+        outdated[name] = {
+            current: "2.0.0",
+            wanted: "2.1.1",
+            latest: "2.1.1",
+            location: "node_modules/" + name,
+        };
+    }
+    for (i = 0; i < breaking; i++) {
+        const name = "updtr-test-module-" + i;
+
+        outdated[name] = {
+            current: "1.0.0",
+            wanted: "1.1.1",
+            latest: "2.0.0",
+            location: "node_modules/" + name,
+        };
+    }
+
     return [
         { stdout: "" }, // installMissing
         // npm exits with exit code 1 when there are outdated dependencies
         new ExecError({
-            stdout: JSON.stringify({
-                "updtr-test-module-1": {
-                    current: "1.0.0",
-                    wanted: "1.1.1",
-                    latest: "2.0.0",
-                    location: "node_modules/updtr-test-module-1",
-                },
-                "updtr-test-module-2": {
-                    current: "2.0.0",
-                    wanted: "2.1.1",
-                    latest: "2.1.1",
-                    location: "node_modules/updtr-test-module-2",
-                },
-                "updtr-test-module-3": {
-                    current: "2.0.0",
-                    wanted: "2.1.1",
-                    latest: "2.1.1",
-                    location: "node_modules/updtr-test-module-3",
-                },
-            }),
+            stdout: JSON.stringify(outdated),
             exitCode: 1,
         }), // outdated
     ];
+}
+
+function updateTaskNames(updateTasks) {
+    return updateTasks.map(({ name }) => name);
 }
 
 beforeAll(() => execResultsReady);
@@ -54,20 +61,18 @@ describe("run()", () => {
 
         expect(updtr.emit.args.shift()).toMatchSnapshot();
     });
+    test("should emit init sequence events", async () => {
+        const updtr = new FakeUpdtr();
+        const eventNames = ["init/start", "init/end"];
+
+        updtr.execResults = npmNoOutdated;
+
+        await run(updtr);
+
+        expect(pickEventNames(eventNames, updtr.emit.args)).toEqual(eventNames);
+    });
     describe("when there are no outdated dependencies", () => {
-        test("should emit init sequence events", async () => {
-            const updtr = new FakeUpdtr();
-            const eventNames = ["init/start", "init/end"];
-
-            updtr.execResults = npmNoOutdated;
-
-            await run(updtr);
-
-            expect(pickEventNames(eventNames, updtr.emit.args)).toEqual(
-                eventNames
-            );
-        });
-        test("should not emit sequentialUpdate events", async () => {
+        test("should not execute the sequential-update", async () => {
             const updtr = new FakeUpdtr();
 
             updtr.execResults = npmNoOutdated;
@@ -76,7 +81,7 @@ describe("run()", () => {
 
             expect(
                 pickEventNames(["sequential-update/start"], updtr.emit.args)
-            ).toEqual([]);
+            ).toHaveLength(0);
         });
         test("should emit an end event of expected shape", async () => {
             const updtr = new FakeUpdtr();
@@ -88,13 +93,86 @@ describe("run()", () => {
             expect(updtr.emit.args.pop()).toMatchSnapshot();
         });
     });
-    describe("when there are outdated dependencies", () => {
-        test("should execute the batch-update for non-breaking updates", async () => {
+    describe("when there is just one non-breaking update", () => {
+        test("should run the sequential-update", async () => {
             const updtr = new FakeUpdtr();
-            const eventNames = ["batch-update/start", "batch-update/end"];
 
-            updtr.execResults = npmOutdated.concat(
+            updtr.execResults = npmOutdated({ nonBreaking: 1 }).concat(
+                updateTestPass
+            );
+
+            await run(updtr);
+
+            const batchUpdateEvents = pickEvents(
+                "batch-update/start",
+                updtr.emit.args
+            );
+            const sequentialUpdateEvent = pickEvents(
+                "sequential-update/start",
+                updtr.emit.args
+            )[0];
+
+            expect(batchUpdateEvents).toHaveLength(0);
+            expect(updateTaskNames(sequentialUpdateEvent.updateTasks)).toEqual([
+                "updtr-test-module-0",
+            ]);
+        });
+        test("should emit an end event of expected shape", async () => {
+            const updtr = new FakeUpdtr();
+
+            updtr.execResults = npmOutdated({ nonBreaking: 1 }).concat(
+                updateTestPass
+            );
+
+            await run(updtr);
+
+            expect(updtr.emit.args.pop()).toMatchSnapshot();
+        });
+    });
+    describe("when there are just breaking updates", () => {
+        test("should run the sequential-update for all dependencies", async () => {
+            const updtr = new FakeUpdtr();
+
+            updtr.execResults = npmOutdated({ breaking: 2 }).concat(
                 updateTestPass,
+                updateTestFail
+            );
+
+            await run(updtr);
+
+            const batchUpdateEvents = pickEvents(
+                "batch-update/start",
+                updtr.emit.args
+            );
+            const sequentialUpdateEvent = pickEvents(
+                "sequential-update/start",
+                updtr.emit.args
+            )[0];
+
+            expect(batchUpdateEvents).toHaveLength(0);
+            expect(updateTaskNames(sequentialUpdateEvent.updateTasks)).toEqual([
+                "updtr-test-module-0",
+                "updtr-test-module-1",
+            ]);
+        });
+        test("should emit an end event of expected shape", async () => {
+            const updtr = new FakeUpdtr();
+
+            updtr.execResults = npmOutdated({ breaking: 2 }).concat(
+                updateTestPass,
+                updateTestFail
+            );
+
+            await run(updtr);
+
+            expect(updtr.emit.args.pop()).toMatchSnapshot();
+        });
+    });
+    describe("when there are two non-breaking updates and the update is ok", () => {
+        test("should only run the batch-update", async () => {
+            const updtr = new FakeUpdtr();
+
+            updtr.execResults = npmOutdated({ nonBreaking: 2 }).concat(
                 updateTestPass
             );
 
@@ -104,41 +182,21 @@ describe("run()", () => {
                 "batch-update/start",
                 updtr.emit.args
             )[0];
-
-            expect(pickEventNames(eventNames, updtr.emit.args)).toEqual(
-                eventNames
-            );
-            expect(batchUpdateEvent.updateTasks).toMatchSnapshot();
-        });
-        test("should execute the sequential-update for breaking updates", async () => {
-            const updtr = new FakeUpdtr();
-            const eventNames = [
-                "sequential-update/start",
-                "sequential-update/end",
-            ];
-
-            updtr.execResults = npmOutdated.concat(
-                updateTestPass,
-                updateTestPass
-            );
-
-            await run(updtr);
-
-            const sequentialUpdateEvent = pickEvents(
+            const sequentialUpdateEvents = pickEvents(
                 "sequential-update/start",
                 updtr.emit.args
-            )[0];
-
-            expect(pickEventNames(eventNames, updtr.emit.args)).toEqual(
-                eventNames
             );
-            expect(sequentialUpdateEvent.updateTasks).toMatchSnapshot();
+
+            expect(updateTaskNames(batchUpdateEvent.updateTasks)).toEqual([
+                "updtr-test-module-0",
+                "updtr-test-module-1",
+            ]);
+            expect(sequentialUpdateEvents).toHaveLength(0);
         });
         test("should emit an end event of expected shape", async () => {
             const updtr = new FakeUpdtr();
 
-            updtr.execResults = npmOutdated.concat(
-                updateTestPass,
+            updtr.execResults = npmOutdated({ nonBreaking: 2 }).concat(
                 updateTestPass
             );
 
@@ -146,70 +204,49 @@ describe("run()", () => {
 
             expect(updtr.emit.args.pop()).toMatchSnapshot();
         });
-        describe("when the batch-update was executed with one dependency and the batch-update fails", () => {
-            test("should run the sequential-update only for breaking dependencies", async () => {
-                const updtr = new FakeUpdtr();
+    });
+    describe("when there are two non-breaking updates and the update is not ok", () => {
+        test("should run the batch-update first and back off to the sequential-update", async () => {
+            const updtr = new FakeUpdtr();
 
-                updtr.execResults = npmOutdated.concat(
-                    updateTestFail,
-                    updateTestPass
-                );
+            updtr.execResults = npmOutdated({ nonBreaking: 2 }).concat(
+                updateTestFail,
+                updateTestFail,
+                updateTestPass
+            );
 
-                await run(updtr);
+            await run(updtr);
 
-                const sequentialUpdateEvent = pickEvents(
-                    "sequential-update/start",
-                    updtr.emit.args
-                )[0];
+            const batchUpdateEvent = pickEvents(
+                "batch-update/start",
+                updtr.emit.args
+            )[0];
+            const sequentialUpdateEvent = pickEvents(
+                "sequential-update/start",
+                updtr.emit.args
+            )[0];
 
-                expect(sequentialUpdateEvent.updateTasks).toMatchSnapshot();
-            });
-            test("should emit an end event of expected shape", async () => {
-                const updtr = new FakeUpdtr();
-
-                updtr.execResults = npmOutdated.concat(
-                    updateTestFail,
-                    updateTestPass
-                );
-
-                await run(updtr);
-
-                expect(updtr.emit.args.pop()).toMatchSnapshot();
-            });
+            expect(updateTaskNames(batchUpdateEvent.updateTasks)).toEqual([
+                "updtr-test-module-0",
+                "updtr-test-module-1",
+            ]);
+            expect(updateTaskNames(sequentialUpdateEvent.updateTasks)).toEqual([
+                "updtr-test-module-0",
+                "updtr-test-module-1",
+            ]);
         });
-        describe("when the batch-update was executed with more than one dependency and the batch-update fails", () => {
-            test("should run the sequential-update for all dependencies", async () => {
-                const updtr = new FakeUpdtr();
+        test("should emit an end event of expected shape", async () => {
+            const updtr = new FakeUpdtr();
 
-                updtr.execResults = npmOutdatedBatchUpdate().concat(
-                    updateTestFail,
-                    updateTestFail,
-                    updateTestPass,
-                    updateTestPass
-                );
+            updtr.execResults = npmOutdated({ nonBreaking: 2 }).concat(
+                updateTestFail,
+                updateTestFail,
+                updateTestPass
+            );
 
-                await run(updtr);
+            await run(updtr);
 
-                const sequentialUpdateEvent = pickEvents(
-                    "sequential-update/start",
-                    updtr.emit.args
-                )[0];
-
-                expect(sequentialUpdateEvent.updateTasks).toMatchSnapshot();
-            });
-            test("should emit an end event of expected shape", async () => {
-                const updtr = new FakeUpdtr();
-
-                updtr.execResults = npmOutdated.concat(
-                    updateTestFail,
-                    updateTestFail,
-                    updateTestPass
-                );
-
-                await run(updtr);
-
-                expect(updtr.emit.args.pop()).toMatchSnapshot();
-            });
+            expect(updtr.emit.args.pop()).toMatchSnapshot();
         });
     });
 });
