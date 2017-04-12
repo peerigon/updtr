96800df962b9a51eb79434b8addf46b10c650232
Johannes Ewald <johannes.ewald@peerigon.com>
2017-04-12 16:08:25 +0200

    Refactor createUpdateTask

--- src/tasks/util/createUpdateTask.js
+++ src/tasks/util/createUpdateTask.js
@@ -1,7 +1,35 @@
-export default function createUpdateTask(outdated, { nonBreaking }) {
+import semver from "semver";
+import {
+    UPDATE_TO_LATEST,
+    UPDATE_TO_NON_BREAKING,
+    UPDATE_TO_WANTED,
+} from "../../constants/config";
+
+function determineUpdateToVersion({ updateTo }, { current, wanted, latest }) {
+    switch (updateTo) {
+        case UPDATE_TO_LATEST:
+            return latest;
+        case UPDATE_TO_WANTED:
+            return wanted;
+        case UPDATE_TO_NON_BREAKING:
+    }
+
+    const range = "^" + current;
+
+    if (semver.satisfies(latest, range) === true) {
+        return latest;
+    }
+    if (semver.satisfies(wanted, range) === true) {
+        return wanted;
+    }
+
+    return current;
+}
+
+export default function createUpdateTask(outdated, updtrConfig) {
     return {
         name: outdated.name,
-        updateTo: nonBreaking === true ? outdated.wanted : outdated.latest,
+        updateTo: determineUpdateToVersion(updtrConfig, outdated),
         rollbackTo: outdated.current,
     };
 }
--- test/tasks/util/createUpdateTask.test.js
+++ test/tasks/util/createUpdateTask.test.js
@@ -1,30 +1,88 @@
+import semver from "semver";
 import createUpdateTask from "../../../src/tasks/util/createUpdateTask";
+import {
+    UPDATE_TO_LATEST,
+    UPDATE_TO_NON_BREAKING,
+    UPDATE_TO_WANTED,
+} from "../../../src/constants/config";
+import FakeUpdtr from "../../helpers/FakeUpdtr";
+import outdateds from "../../fixtures/outdateds";
 
-const baseOutdated = {
-    name: "some-module",
-    current: "1.0.0",
-    latest: "2.0.0",
-    wanted: "1.8.0",
-};
-const baseUpdtrConfig = {
-    updateTo: "latest",
-};
+function stringify(outdated) {
+    return [
+        "current " + outdated.current,
+        "wanted " + outdated.wanted,
+        "latest " + outdated.latest,
+    ].join(", ");
+}
+
+expect.extend({
+    toSatisfySemver(received, argument) {
+        const pass = semver.satisfies(received, argument);
+
+        if (pass === true) {
+            return {
+                message: () =>
+                    `expected ${ received } not to satisfy ${ argument }`,
+                pass,
+            };
+        }
+
+        return {
+            message: () => `expected ${ received } to satisfy ${ argument }`,
+            pass,
+        };
+    },
+});
 
 describe("createUpdateTask()", () => {
     test("should return a valid update task", () => {
         expect(
-            createUpdateTask(baseOutdated, baseUpdtrConfig)
+            createUpdateTask(outdateds[0], {
+                ...FakeUpdtr.baseConfig,
+                updateTo: UPDATE_TO_LATEST,
+            })
         ).toMatchSnapshot();
     });
-    describe("if the nonBreaking flag is set", () => {
-        test("should return a valid update task", () => {
-            const updtrConfig = { ...baseUpdtrConfig };
+    describe(`when updateTo is "${ UPDATE_TO_LATEST }"`, () => {
+        outdateds.forEach(outdated => {
+            test(`when given ${ stringify(outdated) } should update to ${ outdated.latest } and rollback to ${ outdated.current }`, () => {
+                const updateTask = createUpdateTask(outdated, {
+                    ...FakeUpdtr.baseConfig,
+                    updateTo: UPDATE_TO_LATEST,
+                });
 
-            updtrConfig.nonBreaking = true;
+                expect(updateTask.updateTo).toBe(outdated.latest);
+                expect(updateTask.rollbackTo).toBe(outdated.current);
+            });
+        });
+    });
+    describe(`when updateTo is "${ UPDATE_TO_NON_BREAKING }"`, () => {
+        outdateds.forEach(outdated => {
+            const range = "^" + outdated.current;
 
-            expect(
-                createUpdateTask(baseOutdated, updtrConfig)
-            ).toMatchSnapshot();
+            test(`when given ${ stringify(outdated) } should return an update that satisfies ${ range } and rollback to ${ outdated.current }`, () => {
+                const updateTask = createUpdateTask(outdated, {
+                    ...FakeUpdtr.baseConfig,
+                    updateTo: UPDATE_TO_NON_BREAKING,
+                });
+
+                expect(updateTask.updateTo).toSatisfySemver(range);
+                expect(updateTask.rollbackTo).toBe(outdated.current);
+            });
+        });
+    });
+    describe(`when updateTo is "${ UPDATE_TO_WANTED }"`, () => {
+        outdateds.forEach(outdated => {
+            test(`when given ${ stringify(outdated) } should update to ${ outdated.wanted } and rollback to ${ outdated.current }`, () => {
+                const updateTask = createUpdateTask(outdated, {
+                    ...FakeUpdtr.baseConfig,
+                    updateTo: UPDATE_TO_WANTED,
+                });
+
+                expect(updateTask.updateTo).toBe(outdated.wanted);
+                expect(updateTask.rollbackTo).toBe(outdated.current);
+            });
         });
     });
 });
